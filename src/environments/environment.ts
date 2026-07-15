6653323064b2eb79d128553a7d9e631f09032a22
ashiwoo <ashiwoo269@gmail.com>
2026-07-16 02:41:39 +0530

    uncomment the live env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:5000/api'
-  apiUrl: 'https://bicxo-sprint-board.onrender.com/api'
+  apiUrl: 'http://localhost:5000/api'
+  // apiUrl: 'https://bicxo-sprint-board.onrender.com/api'
 };
